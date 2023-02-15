ff81879ff6a078c6586cb1494a7ee1527d65b638
AbidielChagas <abidielchagas@outlook.com>
2023-02-15 17:26:44 -0300

    Page-not-found created, closes #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
      RouterModule.forRoot([
       {path: '', component: HomeComponent},
+      {path: '**', component: PageNotFoundComponent}
   ])
   ],
   providers: [],
